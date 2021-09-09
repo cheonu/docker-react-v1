5a62805f81a76314d2afa0b0f4068600caad30d2
Bamiche Onu <cheonu85@gmail.com>
2021-09-09 08:40:10 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Son Oroh.
+          Hi Son Oroh, first day at school.
         </p>
         <a
           className="App-link"
